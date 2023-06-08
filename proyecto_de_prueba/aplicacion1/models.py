49f5a9a8ad36ef2405be20b5ff043d7d7ae5bb4f
Leonardo Rodenas Escobar <83415965+Leonardo-Rodenas@users.noreply.github.com>
2023-06-08 17:56:22 -0400

    actualizo proyecto

---
+++ proyecto_de_prueba/aplicacion1/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    nombre_usuario = models.CharField(max_length=100, unique=True, default='usuario')
+    correo = models.EmailField()
+    telefono = models.CharField(max_length=20)
+    imagen_perfil = models.ImageField(upload_to='static/img/perfil/', blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre_usuario 
