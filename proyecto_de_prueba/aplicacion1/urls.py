e9949d1f6484aebb58252fe9068d77a611c256c9
Leonardo Rodenas Escobar <83415965+Leonardo-Rodenas@users.noreply.github.com>
2023-06-15 23:04:26 -0400

    Delete proyecto_de_prueba directory

--- proyecto_de_prueba/aplicacion1/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from aplicacion1.views import lista_usuarios, registro_usuario
-
-urlpatterns = [
-    path('usuarios/', lista_usuarios, name='lista_usuarios'),
-    path('formulario/', registro_usuario, name='formulario'),
-]
